01cb29bad52d893ea58f1058ec097c5d6c8b8605
limujun1999 <145745700+limujun1999@users.noreply.github.com>
2026-05-04 22:10:11 +0800

    Revert "Merge branch 'main' of https://github.com/YIfanZHao0922/CPT202-Groupcoursework"
    This reverts commit 0c435687dc2f7f6bc9e1a7dba9fba83ccd2a8d4a, reversing
    changes made to b0b4608918235f6cd021c7d26030c273c398d55c.

--- src/main/java/com/cpt202/pss/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cpt202.pss.exception;
-
-public class ResourceNotFoundException extends BusinessException {
-    public ResourceNotFoundException(String message) {
-        super(404, message);
-    }
-}
